1568ca2401fc9c6617e0809a0a33053b6fa6c692
billerjully <billerjully@gmail.com>
2024-07-24 21:28:26 +0300

    added some design for personal info block

--- reactjs/src/components/Personal.jsx
+++ reactjs/src/components/Personal.jsx
@@ -1,21 +1,59 @@
 import './personal.css'
+import userImage from '../files/user-img.jpg'
+import tg from '../files/tg.svg'
+import lin from '../files/in.svg'
+
 const Personal = () => {
     return (
         <section className="personal">
-            <h1>This is personal</h1>
+            <h1>Software engineer</h1>
             <div className="img-area">
-                <img src="" alt="" />
+                <img src={userImage} alt="" />
             </div>
             <div className="personal-description">
                 <h2>Bulat Zinnurov</h2>
-                <div className="personal-description-block">24 y.o.</div>
-                <div className="personal-description-block">Kazan</div>
+                <div className="personal-description-block">
+                    <span className="info-key">Age:</span>
+                    <span className="info-value">24 y.o.</span>
+                </div>
+                <div className="personal-description-block">
+                    <span className="info-key">Location:</span>
+                    <span className="info-value">Kazan</span>
+                </div>
+                <div className="personal-description-block">
+                    <span className="info-key">Experience:</span>
+                    <span className="info-value">2 years</span>
+                </div>
             </div>
             <div className="personal-social-links">
-                <div className="personal-social-link">num1</div>
-                <div className="personal-social-link">num2</div>
-                <div className="personal-social-link">num3</div>
-                <div className="personal-social-link">num4</div>
+                <h3>Social links</h3>
+                <div className="personal-social-links-block">
+                    <div className="personal-social-link">
+                        <a href="" className="social-link-a">
+                            <img src={tg} className="link-img" alt="" />
+                        </a>
+                    </div>
+                    <div className="personal-social-link">
+                        <a href="" className="social-link-a">
+                            <img
+                                src={lin}
+                                className="link-img"
+                                alt=""
+                                title="GitHub"
+                            />
+                        </a>
+                    </div>
+                    <div className="personal-social-link">
+                        <a href="" className="social-link-a">
+                            <img src="" alt="" />
+                        </a>
+                    </div>
+                    <div className="personal-social-link">
+                        <a href="" className="social-link-a">
+                            <img src="" alt="" />
+                        </a>
+                    </div>
+                </div>
             </div>
         </section>
     )
