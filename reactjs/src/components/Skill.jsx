fdcad91fbfd3897153ac8fadaa4283ae501ddb17
billerjully <billerjully@gmail.com>
2024-07-27 22:07:26 +0300

    app.css crash fix

--- reactjs/src/components/Skill.jsx
+++ reactjs/src/components/Skill.jsx
@@ -7,10 +7,10 @@ export default function Skill() {
     const labels = ['1', '2', '3', '4', '5', '6']
     return (
         <div className="skill-block">
-            <div className="skills-container"></div>
             <div className="skills-map">
                 <RadarChart labels={labels} data={data} />
             </div>
+            <div className="skills-container"></div>
         </div>
     )
 }
