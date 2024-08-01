30b743bb7c3c560b72daa6108398d9d4334c5ca6
billerjully <billerjully@gmail.com>
2024-08-01 09:58:12 +0300

    added some data import

--- reactjs/src/components/Portfolio.jsx
+++ reactjs/src/components/Portfolio.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import Skill from './Skill'
 import Project from './Project'
+import projects from '../files/projects.json'
+import skills from '../files/skills.json'
 
 import './portfolio.css'
 export default function Portfolio() {
@@ -15,6 +17,7 @@ export default function Portfolio() {
                     Hi there! I am Web Developer! Hi there! I am Web Developer!
                     Hi there! I am Web Developer! Hi there! I am Web Developer!
                     Hi there! I am Web Developer! Hi there! I am Web Developer!
+                    с большим интересом к созданию крутых приложений.
                 </p>
             </div>
             <div className="education-container">
@@ -31,16 +34,16 @@ export default function Portfolio() {
                     </span>
                 </div>
             </div>
-            <div className="tech-and-skills-container">
+            <div className="tech-and-skills-container ">
                 <h3>Skills</h3>
-                <Skill />
+                <Skill skillData={skills} />
             </div>
             <div className="projects-container">
                 <h3>Projects</h3>
                 <div className="projects">
-                    <Project />
-                    <Project />
-                    <Project />
+                    {projects.map((project, index) => (
+                        <Project key={index} projectData={project} />
+                    ))}
                 </div>
             </div>
         </section>
--- reactjs/src/components/Skill.jsx
+++ reactjs/src/components/Skill.jsx
@@ -2,14 +2,18 @@ import React from 'react'
 import './skill.css'
 import RadarChart from './Radar'
 
-export default function Skill() {
-    const data = [85, 75, 90, 80, 70, 20]
-    const labels = ['1', '2', '3', '4', '5', '6']
+export default function Skill({ skillData }) {
+    const skillQty = skillData.map((item) => item.skillQty)
+    const skill = skillData.map((item) => item.skill)
     return (
         <div className="skill-block">
-            <div className="skills-container"></div>
+            <div className="skills-container">
+                {skill.map((skillName, index) => (
+                    <p key={index}>{skillName}</p>
+                ))}
+            </div>
             <div className="skills-map">
-                <RadarChart labels={labels} data={data} />
+                <RadarChart labels={skill} data={skillQty} />
             </div>
         </div>
     )
